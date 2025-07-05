3e95d45516b00099f9bd46282517044b92dbe4e8
Wode <danielpbea@gmail.com>
2025-07-05 19:05:40 +0300

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+from utils import divide
+
+def run():
+    a = 10
+    b = 0
+    result = divide(a, b)
+    print(f"Result of {a} / {b} is {result}")
+
+if __name__ == "__main__":
+    run()
---
+++ utils.py
@@ -0,0 +1,8 @@
+def divide(x, y):
+    """
+    Returns the integer division of x by y.
+    BUG: when y == 0, this returns None instead of raising an error.
+    """
+    if y == 0:
+        return  
+    return x // y
